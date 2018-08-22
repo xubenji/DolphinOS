151fe7b33fae67fe2d2b6ab4ecc91c00794f67f3
Olaf <459547070@qq.com>
2018-08-21 23:54:26 -0700

    0.07a

--- DolphinOS/kernel/_stack_chk_fail.h
+++ DolphinOS/kernel/_stack_chk_fail.h
@@ -1,6 +1,6 @@
 /*the gcc need to check the stack 
  *it will call this function
- *I didn't cancel the stack protection
+ *I didn't disable the stack protection check of gcc
  *So I wrote this function
  */
 void __stack_chk_fail();
